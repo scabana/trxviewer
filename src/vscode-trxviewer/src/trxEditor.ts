aa9fc6e79e66244b26dd59ced7edf8cb4a0198ed
Simon Le Bourdais-Cabana <simonlbc@gmail.com>
2020-11-18 20:58:25 -0500

    Setup prod and dev for webpack. (#15)
    * Adding config for production
    * Enable source maps in release mode

--- src/vscode-trxviewer/src/trxEditor.ts
+++ src/vscode-trxviewer/src/trxEditor.ts
@@ -1,5 +1,4 @@
 import * as path from 'path';
-import { TextDecoder } from 'util';
 import * as vscode from 'vscode';
 import { getNonce } from './util';
 
@@ -61,30 +60,31 @@ export class TrxEditorProvider implements vscode.CustomTextEditorProvider {
 	 * Get the static html used for the editor webviews.
 	 */
 	private async getHtmlForWebview(webview: vscode.Webview): Promise<string> {
-		// // Local path to script and css for the webview
-		// const scriptUri = webview.asWebviewUri(vscode.Uri.file(
-		// 	path.join(this.context.extensionPath, 'media', 'catScratch.js')
-		// ));
-		const base = webview.asWebviewUri(vscode.Uri.file(
-			path.join(this.context.extensionPath, 'media')
-		));
-
-		// Use a nonce to whitelist which scripts can be run
-
-		const encoder = new TextDecoder("utf-8");
-
-		const index = await vscode.workspace.fs.readFile(vscode.Uri.file(
-			path.join(this.context.extensionPath, 'media', 'vs-code.html')
-		));
-		let indexText = encoder.decode(index);
-
 		const nonce = getNonce();
+		const base = webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, 'media')));
+		const index = await vscode.workspace.fs.readFile(vscode.Uri.file(path.join(this.context.extensionPath, 'media', 'vs-code.html')));
+		let indexText = Buffer.from(index).toString("utf8");
 
 		indexText = indexText.replace("<meta charset=\"utf-8\" />", `<meta charset="UTF-8"><meta http-equiv="Content-Security-Policy" content="default-src 'none';img-src ${webview.cspSource}; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';" />`);
 		indexText = indexText.replace(/<script/gi, `<script nonce="${nonce}"`);
 		indexText = indexText.replace(/<link/gi, `<link nonce="${nonce}"`);
-
 		indexText = indexText.replace("<base href=\"/\" />", `<base href="${base}/" />`);
+
+		if (vscode.workspace.getConfiguration().get("trxviewer.enableSourceMaps") === true) {
+			try {
+				await vscode.workspace.fs.stat(vscode.Uri.file(path.join(this.context.extensionPath, 'media', 'scripts', 'app.debug.js')));
+			}
+			catch {
+				const appjs = await vscode.workspace.fs.readFile(vscode.Uri.file(path.join(this.context.extensionPath, 'media', 'scripts', 'app.js')));
+				const jsString = Buffer.from(appjs).toString("utf8");
+				const appjsmap = await vscode.workspace.fs.readFile(vscode.Uri.file(path.join(this.context.extensionPath, 'media', 'scripts', 'app.js.map')));
+				const mapBase64 = Buffer.from(appjsmap).toString("base64");
+
+				await vscode.workspace.fs.writeFile(vscode.Uri.file(path.join(this.context.extensionPath, 'media', 'scripts', 'app.debug.js')), Buffer.from(jsString + "\r\n" + "//# sourceMappingURL=data:application/json;charset=utf-8;base64," + mapBase64));
+			}
+
+			indexText = indexText.replace("app.js", "app.debug.js");
+		}
 
 		return indexText;
 	}
